d28a29bdd2331213abdcf6415dbf0d2c74deed02
Sombamanya <didomukendi@gmail.com>
2021-06-17 12:19:38 -0700

    fix documentation rules in 4-puts.c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
-int main ()
+/**
+ * main - The entry point of my program
+ *
+ * Return: Return O if there is no error in main program
+ */
+int main(void)
 {
-   	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
