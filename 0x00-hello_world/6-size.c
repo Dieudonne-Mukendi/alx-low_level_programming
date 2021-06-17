babd15f181dac57bd8e4126ac7535e42d8229c56
Sombamanya <didomukendi@gmail.com>
2021-06-17 13:13:53 -0700

    fix 6-size.c program code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,15 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)", sizeof(char));
-	Printf("Size of an int: %d byte(s)", sizeof(int));
-	printf("Size of a long int: %d byte(s)", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
-	printf("Size of a float: %d byte(s)", sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %d byte(s)", sizeof(a));
+	Printf("Size of an int: %d byte(s)", sizeof(b));
+	printf("Size of a long int: %d byte(s)", sizeof(c));
+	printf("Size of a long long int: %d byte(s)", sizeof(d));
+	printf("Size of a float: %d byte(s)", sizeof(e));
 	return (0);
 }
