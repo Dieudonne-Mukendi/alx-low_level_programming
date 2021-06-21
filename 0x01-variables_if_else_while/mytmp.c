d093a653691d72f7953db467b78bc4a496d58387
Sombamanya <didomukendi@gmail.com>
2021-06-21 01:36:31 -0700

    fix 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/mytmp.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ *main - Prints combinations of decimal digits using only putchar
+ *
+ *Description: Prints combinations of decimal digits using only putchar
+ *Return: 0
+ */
+int main(void)
+{
+	int d_one = 0;
+	int d_two = 0;
+	int d_three = 0;
+	int d_four = 0;
+
+	for (d_one = 48; d_one <= 57; d_one++)
+	{
+		for (d_two = 48; d_two <= 56; d_two++)
+		{
+			for (d_three = d_one; d_three <= 57; d_three++)
+			{
+				for (d_four = 48; d_four <= 57; d_four++)
+				{
+					if (!(d_four == d_two && d_three != 0))
+					{
+						putchar(d_one);
+						putchar(d_two);
+						putchar(' ');
+						putchar(d_three);
+						putchar(d_four);
+						if (!((d_one == 57 && d_two == 56) && (d_three == 57 && d_four == 57)))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
