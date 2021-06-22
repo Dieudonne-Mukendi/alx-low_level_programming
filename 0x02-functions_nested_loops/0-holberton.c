dd236c19de215fdd3ae3bf705a2704c438a3aa27
Sombamanya <didomukendi@gmail.com>
2021-06-22 02:40:11 -0700

    add 0-holberton.c

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * main - The entry point of my program
+ *
+ * Description: The program prints Holberton
+ * followed by a new line.
+ *
+ * Return: Return O if there is no error in main program
+ */
+int main(void)
+{
+	int n = 0;
+	char holberton[15] = "Holberton";
+
+	while (holberton[n] != '\0')
+	{
+		_putchar(holberton[n]);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
