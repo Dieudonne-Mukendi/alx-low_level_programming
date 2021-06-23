269fb12537fca1b55af594acaab959d305b1f6cb
Sombamanya <didomukendi@gmail.com>
2021-06-22 18:37:35 -0700

    add times_table program

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * times_table - prints 9 times table, starting with 0.
+ *
+ * Description - Does not receive any parameters
+ *
+ * Return: Does not return anything
+ */
+void times_table(void)
+{
+	int a = 0, b = 0, c = 0, d = 0;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if (b <= 9 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (c <= 9)
+					_putchar(' ');
+			}
+			if (c <= 9)
+				_putchar(c + 48);
+			else
+			{
+				d = c / 10;
+				_putchar(d + 48);
+				_putchar((c % 10) + 48);
+			}
+		}
+	}
+}
