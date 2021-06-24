7f58a516ab56449c3d823e09f911b722d49789e9
Sombamanya <didomukendi@gmail.com>
2021-06-24 16:03:30 -0700

    add 9-fizz_buzz.c program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * main  - the entry point
+ *
+ * Return: 0 if the program run with success
+ */
+int main(void)
+{
+	int a = 0, b = 0, c = 0;
+	for (a = 1; a < 101; a++)
+	{
+		b = a % 3;
+		c = a % 5;
+		if (b == 0 && c == 0)
+			printf("FizzBuzz ");
+		else if (c == 0 %% b != 0)
+			printf("Buzz ");
+		else if (b == 0 && c != 0)
+			printf("Fizz ");
+		else
+			printf("%d ", a);
+	}
+	putchar('\n');
+
+	return (0);
+}
