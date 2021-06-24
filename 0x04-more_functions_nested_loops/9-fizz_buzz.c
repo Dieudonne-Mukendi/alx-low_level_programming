73e4f610ea1c2a4c9613aa022824cdc3e4ba84a3
Sombamanya <didomukendi@gmail.com>
2021-06-24 16:07:30 -0700

    fix 9-fizz_buzz.c program

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 	int a = 0, b = 0, c = 0;
+
 	for (a = 1; a < 101; a++)
 	{
 		b = a % 3;
 		c = a % 5;
 		if (b == 0 && c == 0)
 			printf("FizzBuzz ");
-		else if (c == 0 %% b != 0)
+		else if (c == 0 && b != 0)
 			printf("Buzz ");
 		else if (b == 0 && c != 0)
 			printf("Fizz ");
