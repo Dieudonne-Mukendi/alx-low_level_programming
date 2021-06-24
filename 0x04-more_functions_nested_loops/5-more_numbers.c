009fe7d514d9e9a2436c17cd2a6a2e80b87c7d49
Sombamanya <didomukendi@gmail.com>
2021-06-24 13:48:53 -0700

    fix for loop in 5-more_functions.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,23 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k, l;
 
 	for (j = 0; j < 10; j++)
 	{
+		k = 0;
+		l = 0;
 		for (i = 48; i < 63; i++)
 		{
-			_putchar(i);
+			if (!(i > 57))
+				l = i;
+			else
+			{
+				l = i - 10;
+				k = 49;
+				_putchar(k);
+			}
+			_putchar(l);
 		}
 		_putchar('\n');
 	}
