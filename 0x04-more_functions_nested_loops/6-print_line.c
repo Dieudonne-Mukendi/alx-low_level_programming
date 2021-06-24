c3aabb570fe5996b7fc96c7aa8143e9011c0008f
Sombamanya <didomukendi@gmail.com>
2021-06-24 14:05:38 -0700

    add 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_line - A function who print lines n times
+ *
+ * @n: number of times to print line
+ *
+ * Return: Does not return anything
+ */
+void print_numbers(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
