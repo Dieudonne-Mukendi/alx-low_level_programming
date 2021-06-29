ed66be7a3e4a5933f09214043c118a7e0c434cd0
Sombamanya <didomukendi@gmail.com>
2021-06-29 05:05:00 -0700

    fix 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,10 +12,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	do {
+	while (*(src + i) != '\0')
+	{
 		*(dest + i) = *(src + i);
 		i++;
-	} while (*(src + i) != '\0');
+	}
 	dest[i] = '\0';
 	return (dest);
 }
