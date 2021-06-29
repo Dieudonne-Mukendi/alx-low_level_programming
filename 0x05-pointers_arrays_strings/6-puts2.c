fe29f14be1d88a9776619200f0fbfce2ef83d2da
Sombamanya <didomukendi@gmail.com>
2021-06-29 01:03:36 -0700

    add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other character then a new line.
+ * @str: string to print
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	char *p = str;
+
+	while (str[i] != '\0')
+	{
+		if (!(i % 2) || i == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
