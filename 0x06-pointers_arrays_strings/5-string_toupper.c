3a241167c23d36f857701a07989c61215bd1d070
Sombamanya <didomukendi@gmail.com>
2021-07-01 07:56:23 -0700

    add 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string to change
+ * Return: string changed
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str != '\0')
+	{
+		if (str[i] > 96 && str[i] < 123)
+			str[i] = str[i] - 32;
+		i++;
+	}
+
+	return (str);
+}
