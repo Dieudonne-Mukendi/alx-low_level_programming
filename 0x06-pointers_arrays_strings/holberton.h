e1633542d47e6d3264a557830e5cd60dbe46d73c
Sombamanya <didomukendi@gmail.com>
2021-07-01 13:48:50 -0700

    add 100-rot13.c

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -7,4 +7,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+char *rot13(char *);
