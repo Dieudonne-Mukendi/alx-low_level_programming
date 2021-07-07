427b9c4669b62f88da949150ecad2b77e70eea59
Sombamanya <didomukendi@gmail.com>
2021-07-07 04:50:59 -0700

    add 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ *
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+}
