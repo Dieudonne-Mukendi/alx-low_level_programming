0e971a2ed9652336259d8be07f9e421fd4af55d4
Sombamanya <didomukendi@gmail.com>
2021-07-07 05:53:23 -0700

    Revert "fix 1-print_rev_recursion.c"
    This reverts commit 24586bbcff5f44647c6a1fa92c0762bf9671ecc3.
    renverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,27 +1,15 @@
 #include "holberton.h"
 /**
- * _print_rev - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: string to print
  *
  * Return: nothing
  */
-void _print_rev(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_print_rev(++s);
+		_print_rev_recursion(++s);
 		_putchar(*s);
 	}
-}
-
-/**
- * _print_rev_recursion - prints string in reverse
- * followed by a new line
- * @s: string to print
- * Return: nothing
- */
-void _print_rev_recursion(char *s)
-{
-	_print_rev(s);
-	_putchar('\n');
 }
