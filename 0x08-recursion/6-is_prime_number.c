bff3c734bf4ef4b52a0a596dd29fdc093bb4fc22
Sombamanya <didomukendi@gmail.com>
2021-07-07 13:45:10 -0700

    add 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * prime_checker - check if @x is prime or composite
+ * @x: number to check
+ * @y: half of @x to help the checking process
+ * Return: 1 if @x is prime otherwise 0
+ */
+int prime_checker(int x, int y)
+{
+	if (y == 1)
+		return (1);
+	else if (y != 1 && x % y == 0)
+		return (0);
+	else
+		prime_checker(x, y - 1);
+}
+
+/**
+ * is_prime_number - returns 1 if the input is a prime number, otherwise 0
+ * @n: input number
+ * Return: 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if (n > 1)
+		return (prime_checker(n, n / 2));
+	else
+		return (0);
+}
