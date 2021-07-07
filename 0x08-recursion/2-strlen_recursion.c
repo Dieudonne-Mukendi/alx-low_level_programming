a73595cb3963db2a7d41f42cf10994fe94a1d71c
Sombamanya <didomukendi@gmail.com>
2021-07-07 06:15:00 -0700

    add 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to check the length
+ *
+ * Return: the length of the string as int
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 1;
+
+	if (*s)
+	{
+		len += _strlen_recursion(s+1);
+		return (len);
+	}
+	else
+	{
+		len = 0;
+		return (len);
+	}
+}
