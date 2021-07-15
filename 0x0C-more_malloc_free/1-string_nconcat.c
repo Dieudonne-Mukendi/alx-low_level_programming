789ec03fd8ac1126d11d77a2b34603a30929c815
Sombamanya <didomukendi@gmail.com>
2021-07-15 13:57:32 -0700

    fixed 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,58 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- *  @n: number of character from second string
- * Return: a pointer to the new string
+ * _strlen - returns the lenght of a string
+ *@s: poiter of character
+ *Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	unsigned int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+/**
+ * *string_nconcat - concatenates two strings
+ *@s1: first string to concatenate
+ *@s2: second string to concatenate
+ *@n: number of bytes to concatenate
+ *Return: the pointer concatenate or null
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int l1, i, j;
 	char *str;
-	unsigned int i, un, len1, len2, slen = 0;
 
-	if (s1)
-	{
-		for (i = 0; s1[i]; i++)
-			;
-	}
-	else
+	/**verify if s1 or s2 is null*/
+	if (s1 == NULL)
 		s1 = "";
-	len1 = (s1) ? i : 0;
-	if (s2)
-	{
-		for (i = 0; s2[i]; i++)
-			;
-	}
-	else
+	if (s2 == NULL)
 		s2 = "";
-	len2 = (s2) ? i : 0;
-	if (n < len2)
-		len2 = n;
-	un = (s1 == "" && s2 == "") ? 0 : 1;
-	str = malloc((len1 + (len2 * sizeof(s2)) + un) * sizeof(*str));
+
+	/**calculate the lenght of the strings*/
+	l1 = _strlen(s1);
+
+	/**asign the memorty to the pointer*/
+	str = malloc((l1 + (n * sizeof(*s2) + 1)) * sizeof(*str));
+
+	/**Verify if the memory is avaiable*/
 	if (str == NULL)
-	{
-		free(str);
 		return (NULL);
+
+	/**Concatenate the string*/
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		str[i] = s1[i];
 	}
-	for (i = 0; i < len1; i++)
-		str[slen++] = s1[i];
-	for (i = 0; i < len2; i++)
-		str[slen++] = s2[i];
-	str[slen] = '\0';
+
+	for (j = 0; s2[j] != '\0' && j < n; j++, i++)
+	{
+		str[i] = s2[j];
+	}
+	str[i] = '\0';
 	return (str);
 }
