5a3c482cb3d544f1e7c721debd03a77d22e6477f
Sombamanya <didomukendi@gmail.com>
2021-07-14 20:25:08 -0700

    fix 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ * cnt_char - count character of a words
+ * @str: words
+ * Return: the number of character
+ */
+int cnt_char(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 32 && str[i] != 9; i++)
+		;
+	return (i);
+}
+/**
  * strtow - splits a string to words
  * @str: string to split *
  * Return: a point to an array of strings or NULL
@@ -9,7 +22,7 @@
 char **strtow(char *str)
 {
 	char **arr_words = NULL;
-	int i, j, maxlen, wlen = 0, slen, words = 0, sig = 0, pre_sig = 0;
+	int i, j = 0, wlen, slen, words = 0, sig = 0, pre_sig = 0;
 
 	if (str == NULL)
 		return (NULL);
@@ -18,9 +31,6 @@ char **strtow(char *str)
 	{
 		sig = (str[i] == 32 || str[i] == '\t') ? 0 : 1;
 		words = (pre_sig == 0 && sig == 1) ? words + 1 : words;
-		if (wlen > 0 && sig == 0)
-			maxlen = (wlen > maxlen) ? wlen : maxlen;
-		wlen = (sig) ? wlen + 1 : 0;
 		pre_sig = sig;
 	}
 	if (words == 0)
@@ -31,27 +41,28 @@ char **strtow(char *str)
 		free(arr_words);
 		return (NULL);
 	}
-	for (i = 0; i < words; i++)
-	{
-		arr_words[i] = (char *)malloc((maxlen + 1) * sizeof(char));
-		if (arr_words[i] == NULL)
-		{
-			for (j = 0; j < i; j++)
-				free(arr_words[j]);
-			free(arr_words);
-			return (NULL);
-		}
-	}
-	wlen = 0;
 	words = 0;
 	for (i = 0; i < slen; i++)
 	{
 		sig = (str[i] == 32 || str[i] == 9) ? 0 : 1;
-		words = (pre_sig == 0 && sig == 1) ? words + 1 : words;
 		if (sig)
-			arr_words[words][wlen] = str[i];
-		wlen = (sig) ? wlen + 1 : 0;
-		pre_sig = sig;
+		{
+			wlen = cnt_char(str + i);
+			arr_words[words] = (char *)malloc((wlen + 1) * sizeof(char));
+			if (arr_words[words] == NULL)
+			{
+				for (; words >= 0; words--)
+					free(arr_words[words]);
+				free(arr_words);
+				return (NULL);
+			}
+			for (j = 0; j < wlen; j++)
+			{
+				arr_words[words][j] = str[i + j];
+			}
+			words++;
+			i += wlen;
+		}
 	}
 	return (arr_words);
 }
