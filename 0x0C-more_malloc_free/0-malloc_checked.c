36cb1cc390b0b1d8e025bec6ddfd0774da10bfa3
Sombamanya <didomukendi@gmail.com>
2021-07-15 08:57:55 -0700

    add 3-array_range.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,6 +11,6 @@ void *malloc_checked(unsigned int b)
 
 	p = malloc(b);
 	if (p == NULL)
-		exit (98);
+		exit(98);
 	return (p);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,7 +14,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int *p;
 	int i;
 
-	if (nmemb == 0 || size == 0 )
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * sizeof(size));
 	if (p == NULL)
@@ -23,6 +23,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 	for (i = 0; i < nmemb * size; i++)
-		*(p+i) = 0;
+		*(p + i) = 0;
 	return (p);
 }
