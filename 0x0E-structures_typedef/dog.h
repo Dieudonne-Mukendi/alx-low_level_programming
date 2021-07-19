aaaad355a9cb18c53127cbd475e79fd688d33002
Sombamanya <didomukendi@gmail.com>
2021-07-19 11:47:06 -0700

    add dog_t typedef in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,12 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
