1c64049337b336d79a2a3a837767afc82b3a689f
Sombamanya <didomukendi@gmail.com>
2021-07-19 13:36:12 -0700

    fix 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,26 +1,81 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * new_dog - creates a new dog
- * @name: the name to set
- * @age: age to set
- * @owner: owner to set
- *
- * Return: a pointer to a new dog
+ * _strlen - returns the lenght of a string
+ *@s: poiter of character
+ *Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * *_strcpy - copies the string pointed
+ *@dest: copie of the string
+ *@src: origin of the string
+ *Return: dest destine copie
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * new_dog - function that creates a new dog
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: name of owner of the dog
+ *Return: dog created
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	/* declare a new dog */
-	dog_t *d;
+	dog_t *new_dog;
+	char *cname, *cowner;
 
-	d = malloc(sizeof(dog_t));
-	/* I verifie if @d is not null */
-	if (d == NULL)
+	new_dog = malloc(sizeof(struct dog));
+
+	if (new_dog == NULL)
 		return (NULL);
-	/*since d is not NULL I initialize */
-	d->name = (name) ? name : NULL;
-	if (age)
-		d->age = age;
-	d->owner = (owner) ? owner : NULL;
-	return (d);
+
+	cname = malloc(sizeof(char *) * (_strlen(name) + 1));
+
+	if (cname == NULL)
+	{
+		free(cname);
+		free(new_dog);
+		return (NULL);
+	}
+
+	cname = _strcpy(cname, name);
+
+	cowner = malloc(sizeof(char *) * (_strlen(owner) + 1));
+	if (cowner == NULL)
+	{
+		free(cowner);
+		free(cname);
+		free(new_dog);
+		return (NULL);
+	}
+
+	cowner = _strcpy(cowner, owner);
+
+	new_dog->name = cname;
+	new_dog->age = age;
+	new_dog->owner = cowner;
+
+	return (new_dog);
 }
