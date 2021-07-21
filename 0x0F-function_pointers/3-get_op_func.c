5080eca8e53f39fa0eb9690ca870ca9d8c30558a
Sombamanya <didomukendi@gmail.com>
2021-07-21 14:34:30 -0700

    fix 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,7 +18,7 @@ int (*get_op_func(char *s))(int a, int b)
 	};
 	int i = 0;
 
-	while (i < 5 && s != NULL)
+	while ((i < 5 && s != NULL) && (s[1] == '\0'))
 	{
 		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
