3dcd410e2e8b4b207c6c7fc223007f372a632767
Sombamanya <didomukendi@gmail.com>
2021-07-21 08:05:26 -0700

    improve 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,6 +8,7 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
+	/* @p is a function pointer that point to the given operation */
 	int (*p)(int, int);
 
 	if (argc != 4)
@@ -15,20 +16,19 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 	p = get_op_func(argv[2]);
 	if (p == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2][0] == '/' && argv[3][0] == '0') ||
-	    (argv[2][0] == '%' && argv[3][0] == '0'))
+	if ((p == op_div || p == op_mod) && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 	printf("%d\n", p(a, b));
 	return (0);
 }
