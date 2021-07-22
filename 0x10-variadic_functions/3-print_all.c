2b0807d27214fd5ba2cbbf56f3ee011b1700d029
Sombamanya <didomukendi@gmail.com>
2021-07-22 13:15:06 -0700

    add print_all and associated functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,66 @@
 #include "variadic_functions.h"
-#include <string.h>
+/**
+ * p_char - print a character
+ * @arg: argument to print
+ * Return: Nothing
+ */
+void p_char(va_list arg)
+{
+	printf("%c", (char)va_arg(arg, int));
+}
+/**
+ * p_int - print an integer
+ * @arg: argument to print
+ * Return: Nothing
+ */
+void p_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+/**
+ * p_float - print float
+ * @arg: argument to print
+ * Return: Nothing
+ */
+void p_float(va_list arg)
+{
+	printf("%f", (double)va_arg(arg, double));
+}
+/**
+ * p_strings - print strings
+ * @arg: argument to print
+ * Return: Nothing
+ */
+void p_strings(va_list arg)
+{
+	char *s = (char *)va_arg(arg, char *);
+
+	printf("%s", (s) ? s : "(nil)");
+}
+/**
+ * get_printer - select the function associated to the given parameter
+ * @f: type of data to print
+ * Return: a pointer to the associated function or NULL
+ */
+void (*get_printer(char f))(va_list)
+{
+	f_print fp_get[] = {
+		{'c', p_char},
+		{'i', p_int},
+		{'f', p_float},
+		{'s', p_strings},
+		{'\0', NULL},
+	};
+	int i = 0;
+
+	while (i < 5 && f != '\0')
+	{
+		if (fp_get[i].c == f)
+			return (fp_get[i].pf);
+		i++;
+	}
+	return (NULL);
+}
 /**
  *  print_all - prints anything
  * @format: list of types per arguments passed
@@ -7,48 +68,24 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i = 0, len, flag;
+	int i = 0, len;
 	char *print;
+	void (*fun)(va_list);
 	va_list p_arg;
 
+	if (format == NULL)
+		return;
 	len = strlen(format);
 	va_start(p_arg, format);
 	while (i < len)
 	{
-		flag = 1;
-		while (flag && i < len)
-		{
-			switch (format[i])
-			{
-			case 'c':
-				printf("%c", (char)va_arg(p_arg, int));
-				flag = 0;
-				break;
-			case 'i':
-				printf("%d", (int)va_arg(p_arg, int));
-				flag = 0;
-				break;
-			case 'f':
-				printf("%f", (double)va_arg(p_arg, double));
-				flag = 0;
-				break;
-			case 's':
-				print = (char *)va_arg(p_arg, char*);
-				if (print == NULL)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", print);
-				flag = 0;
-				break;
-			default:
-				flag = 1;
-			}
-			if (i < len - 1 && flag == 0)
-				printf(", ");
-			i++;
-		}
+		fun = get_printer(format[i]);
+		if (fun != NULL)
+			fun(p_arg);
+		if (i < len - 1 && fun != NULL)
+			printf(", ");
+		i++;
+
 	}
 	printf("\n");
 	va_end(p_arg);
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct pt - Struct of printer type
+ *
+ * @c: Type to print
+ * @pf: The function associated
+ */
+typedef struct pt
+{
+	char c;
+	void (*pf)(va_list);
+} f_print;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
