f2f90b0a8e8c0d87b8028d777d03f4a98973f67e
Sombamanya <didomukendi@gmail.com>
2021-07-22 14:17:36 -0700

    final fix print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,12 +1,53 @@
-#include <string.h>
 #include "variadic_functions.h"
 /**
- * get_printer - select the function associated to the given parameter
- * @f: type of data to print
- * Return: a pointer to the associated function or NULL
+ * p_char - print a character
+ * @arg: argument to print
+ * Return: Nothing
  */
-void (*get_printer(char f))(va_list)
+void p_char(va_list arg)
 {
+	printf("%c", (char)va_arg(arg, int));
+}
+/**
+ * p_int - print an integer
+ * @arg: argument to print
+ * Return: Nothing
+ */
+void p_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+/**
+ * p_float - print float
+ * @arg: argument to print
+ * Return: Nothing
+ */
+void p_float(va_list arg)
+{
+	printf("%f", (double)va_arg(arg, double));
+}
+/**
+ * p_strings - print strings
+ * @arg: argument to print
+ * Return: Nothing
+ */
+void p_strings(va_list arg)
+{
+	char *s = (char *)va_arg(arg, char *);
+
+	printf("%s", (s) ? s : "(nil)");
+}
+/**
+ *  print_all - prints anything
+ * @format: list of types per arguments passed
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	void (*fun)(va_list);
+	va_list p_arg;
 	f_print fp_get[] = {
 		{'c', p_char},
 		{'i', p_int},
@@ -14,40 +55,26 @@ void (*get_printer(char f))(va_list)
 		{'s', p_strings},
 		{'\0', NULL},
 	};
-	int i = 0;
 
-	while (i < 5 && f != '\0')
-	{
-		if (fp_get[i].c == f)
-			return (fp_get[i].pf);
-		i++;
-	}
-	return (NULL);
-}
-/**
- *  print_all - prints anything
- * @format: list of types per arguments passed
- * Return: nothing
- */
-void print_all(const char * const format, ...)
-{
-	int i = 0, len;
-	void (*fun)(va_list);
-	va_list p_arg;
-
-	if (format == NULL)
-		return;
-	len = strlen(format);
 	va_start(p_arg, format);
-	while (i < len)
+	while (format != NULL && format[j] != '\0')
 	{
-		fun = get_printer(format[i]);
-		if (fun != NULL)
-			fun(p_arg);
-		if (i < len - 1 && fun != NULL)
+		i = 0;
+		while (i < 4)
+		{
+			if (fp_get[i].c == format[j])
+			{
+				fun = fp_get[i].pf;
+				break;
+			}
+			i++;
+			fun = NULL;
+		}
+		if (i != 0 && fun)
 			printf(", ");
-		i++;
-
+		if (fun)
+			fun(p_arg);
+		j++;
 	}
 	printf("\n");
 	va_end(p_arg);
