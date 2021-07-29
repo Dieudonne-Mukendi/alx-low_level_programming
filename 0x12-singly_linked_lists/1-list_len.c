ed9901e650fe771d9bb1850e6b841472c54e14e6
Sombamanya <didomukendi@gmail.com>
2021-07-29 13:11:53 -0700

    add list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,23 @@
 #include "lists.h"
+/**
+ * list_len - return the number of nodes
+ * @h: first node
+ *
+ * Return: The number of nodes in a list
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *cnode;
+	size_t i = 0;
+
+	if (h == NULL)
+		return (0);
+	cnode = h;
+	while (cnode != NULL)
+	{
+		cnode = cnode->next;
+		i++;
+	}
+
+	return (i);
+}
