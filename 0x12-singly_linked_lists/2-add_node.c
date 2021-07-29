71ce7974bb27cb42cf8699eaf5b4ead80e4550a1
Sombamanya <didomukendi@gmail.com>
2021-07-29 14:23:36 -0700

    fix conditions in add_node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = (list_t *) malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (new_node == NULL || str == NULL)
 		return (NULL);
 	new_node->str = malloc(sizeof(*str) * strlen(str));
 	if (new_node->str == NULL)
