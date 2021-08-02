ecb2893ca62a278bac80af5924a2109347c06bd0
Sombamanya <didomukendi@gmail.com>
2021-08-02 05:51:25 -0700

    fix betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,7 +16,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = malloc(sizeof(char) * strlen(str));
-	if ((new_node->str == NULL)
+	if (new_node->str == NULL)
 	{
 		free(new_node->str);
 		free(new_node);
