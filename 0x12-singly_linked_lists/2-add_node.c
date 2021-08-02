6a863a2bcad2115c3db30da43e26bd54a1c5a032
Sombamanya <didomukendi@gmail.com>
2021-08-02 03:53:48 -0700

    free node if fails in add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,10 +12,17 @@ list_t *add_node(list_t **head, const char *str)
 
 	new_node = (list_t *) malloc(sizeof(list_t));
 	if (new_node == NULL || str == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 	new_node->str = malloc(sizeof(*str) * strlen(str));
 	if (new_node->str == NULL)
+	{
+		free(new_node->str);
+		free(new_node);
 		return (NULL);
+	}
 	new_node->str = strdup(str);
 	new_node->next = *head;
 	*head = new_node;
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,1 +1,31 @@
 #include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - add a node in the end of the list
+ * @head: a pointer to the first node
+ * @str: string to set to the node
+ * Return: a pointer to the new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *current = head;
+
+	/* we move first to the end */
+	while (current->next != NULL)
+		current = current->next;
+
+	/* now we can add a new node */
+	current->next = (list_t *) malloc(sizeof(list_t));
+	if (current->next == NULL)
+		return (NULL);
+	current->next->str = malloc(sizeof(*str) * strlen(str));
+	if (current->next->str == NULL)
+	{
+		free(current->next);
+		return (NULL);
+	}
+	current->next->str = strdup(str);
+	current->next->next = NULL;
+
+	return (current->next);
+}
