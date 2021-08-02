4d0032bdb74cbf37cd43f3766f6318d48f97d21a
Sombamanya <didomukendi@gmail.com>
2021-08-02 07:25:15 -0700

    add free_list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,20 @@
 #include "lists.h"
+/**
+ *  free_list - deallocate memory to all node of a list
+ *  @head: first element of the list
+ *  Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	if (head == NULL)
+		return;
+	next = head->next;
+	do {
+		free(head->str);
+		free(head);
+		head = next;
+		next = (head) ? head->next : NULL;
+	} while (head != NULL);
+}
