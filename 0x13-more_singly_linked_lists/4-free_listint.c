3e0739e8fe6b5ae1b68f3ae03b534df95a233770
Sombamanya <didomukendi@gmail.com>
2021-08-02 11:27:49 -0700

    add free_listint

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,23 @@
 #include "lists.h"
+/**
+ * free_listint - frees a listint_t list.
+ * @head: pointer to the address of the first element
+ * Return: Nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *h, *p;
+
+	if (head == NULL)
+		return;
+	p = head;
+	h = p->next;
+	do {
+		free(p);
+		p = h;
+		h = h->next;
+	} while (h != NULL);
+	free(p);
+	free(h);
+	free(head);
+}
