9b22b72ee73ff362e42785428059e29df7c499e4
Sombamanya <didomukendi@gmail.com>
2021-08-02 11:20:06 -0700

    free listint2

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,1 +1,30 @@
 #include "lists.h"
+/**
+ * add_nodeint - adds a new element in the last of the list
+ * @head: the first element of the current list
+ * @n: the value to assign to the new element
+ * Return: a pointer to the first element or NULL
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node, *h;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL || head == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		h = *head;
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new_node;
+	}
+	return (*head);
+}
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,1 +1,24 @@
 #include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: pointer to the address of the first element
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *h, *p;
+
+	if (head == NULL)
+		return;
+	p = *head;
+	h = p->next;
+	do {
+		free(*head);
+		if (h != NULL)
+			*head = h;
+		h = h->next;
+	} while (h->next != NULL);
+	free(p);
+	free(h);
+	free(*head);
+}
