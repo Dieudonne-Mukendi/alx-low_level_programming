6f28808665ce84d70b0f2b9403d3f851a7b7ffed
Sombamanya <didomukendi@gmail.com>
2021-08-02 09:46:01 -0700

    add print_listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * print_listint -  prints all the elements of a listint_t list.
+ * @h: head of the list
+ *
+ * Return: The number of elements
+ */
+size_t print_listint(const listint_t *h)
+{
+	listint_t *elem;
+	size_t num = 0;
+
+	if (h == NULL)
+		return (num);
+	elem = malloc(sizeof(listint_t));
+	if (elem == NULL)
+	{
+		free(elem);
+		return (-1);
+	}
+	elem = h;
+	while (elem != NULL)
+	{
+		printf("%d\n", elem->n);
+		num++;
+		elem = elem->next;
+
+	}
+	return (num);
+}
