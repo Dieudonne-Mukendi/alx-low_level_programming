b87adb14c49d20f910cd988ca16f93437ee0da6d
Sombamanya <didomukendi@gmail.com>
2021-08-03 03:15:14 -0700

    fix condition on insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,11 +8,12 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *idx_node = *head;
+	listint_t *new_node, *idx_node;
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (head == NULL ||(*head == NULL && idx != 0))
 		return (NULL);
+	idx_node = *head;
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
