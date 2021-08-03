144eb9e721fa2cc1bbe5c11cea273852b08f268b
Sombamanya <didomukendi@gmail.com>
2021-08-03 03:03:05 -0700

    fix betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *idx_node = *head;
 	unsigned int i = 0;
 
-	if (head == NULL )
+	if (head == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
